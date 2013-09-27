ae597d1f2e8afceb71fab0b3b9c46e98b615252d
Sergey Yevsyukov <huskyserge@gmail.com>
2013-09-27 23:18:35 +0300

    adding JUnit tests for UseDAO

--- src/test/java/knowledgeTest/logic/service/AdminServiceTest.java
+++ src/test/java/knowledgeTest/logic/service/AdminServiceTest.java
@@ -1,7 +1,19 @@
 package knowledgeTest.logic.service;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
 /**
- * User: serge
+ * JUnit test for the {@link knowledgeTest.logic.service.impl.UserServiceImpl} class.
+ * Testing service functions for user role.
+ * ApplicationContext will be loaded from "classpath:/application-context.xml"
  */
-public class AdminServiceTest {
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:application-context.xml"})
+@TransactionConfiguration(defaultRollback = true, transactionManager = "transactionManager")
+public class AdminServiceTest extends AbstractJUnit4SpringContextTests {
 }
--- src/test/java/knowledgeTest/logic/service/UserServiceTest.java
+++ src/test/java/knowledgeTest/logic/service/UserServiceTest.java
@@ -1,7 +1,19 @@
 package knowledgeTest.logic.service;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
 /**
- * User: serge
+ * JUnit test for the {@link knowledgeTest.logic.service.impl.AdminServiceImpl} class.
+ * Testing service functions for admin role.
+ * ApplicationContext will be loaded from "classpath:/application-context.xml"
  */
-public class UserServiceTest {
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:application-context.xml"})
+@TransactionConfiguration(defaultRollback = true, transactionManager = "transactionManager")
+public class UserServiceTest extends AbstractJUnit4SpringContextTests {
 }
